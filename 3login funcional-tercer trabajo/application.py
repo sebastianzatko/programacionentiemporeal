da3a997a205782545e5069843a56dbd1512302ad
sebastianzatko <sebastiantomaszatko@gmail.com>
2018-06-15 03:54:54 -0300

    Commit 15-06
    Agregado ingreso por google y script de generacion de BD en PostgreSQL

--- 3login funcional-tercer trabajo/application.py
+++ 3login funcional-tercer trabajo/application.py
@@ -5,19 +5,150 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from functools import wraps
 from database_setup import Base, User
+from oauth2client.client import flow_from_clientsecrets, FlowExchangeError
 import random
 import string
 import json
 import datetime
 import hashlib
-
+import httplib2
+import requests
 
 app = Flask(__name__)
+
+CLIENT_ID = json.loads(
+		open('client_secrets.json', 'r').read())['web']['client_id']
+
 engine = create_engine('sqlite:///login.db?check_same_thread=false')
 Base.metadata.bind = engine
 
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
+
+
+# GConnect
+@app.route('/gconnect', methods=['POST'])
+def gconnect():
+	print ("Dentro de GConnect")
+		# Validate state token
+	if request.args.get('state') != login_session['state']:
+		response = make_response(json.dumps('Invalid state parameter.'), 401)
+		response.headers['Content-Type'] = 'application/json'
+		return response
+	# Obtain authorization code, now compatible with Python3
+	code = request.data
+
+	try:
+			# Upgrade the authorization code into a credentials object
+		oauth_flow = flow_from_clientsecrets('client_secrets.json', scope='')
+		oauth_flow.redirect_uri = 'postmessage'
+		credentials = oauth_flow.step2_exchange(code)
+	except FlowExchangeError:
+		response = make_response(
+					json.dumps('Failed to upgrade the authorization code.'), 401)
+		response.headers['Content-Type'] = 'application/json'
+		return response
+
+	# Check that the access token is valid.
+	access_token = credentials.access_token
+	url = ('https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=%s'
+				 % access_token)
+	# Submit request, parse response - Python3 compatible
+	h = httplib2.Http()
+	result = json.loads(h.request(url, 'GET')[1])
+
+	# If there was an error in the access token info, abort.
+	if result.get('error') is not None:
+			response = make_response(json.dumps(result.get('error')), 500)
+			response.headers['Content-Type'] = 'application/json'
+			return response
+
+	# Verify that the access token is used for the intended user.
+	gplus_id = credentials.id_token['sub']
+	if result['user_id'] != gplus_id:
+			response = make_response(
+					json.dumps("Token's user ID doesn't match given user ID."), 401)
+			response.headers['Content-Type'] = 'application/json'
+			return response
+
+	# Verify that the access token is valid for this app.
+	if result['issued_to'] != CLIENT_ID:
+			response = make_response(
+					json.dumps("Token's client ID does not match app's."), 401)
+			print ("Token's client ID does not match app's.")
+			response.headers['Content-Type'] = 'application/json'
+			return response
+
+	stored_credentials = login_session.get('credentials')
+	stored_gplus_id = login_session.get('gplus_id')
+	if stored_credentials is not None and gplus_id == stored_gplus_id:
+			response = make_response(json.dumps('Current user is already connected.'),
+																	 200)
+			response.headers['Content-Type'] = 'application/json'
+			return response
+
+	# Store the access token in the session for later use.
+	login_session['access_token'] = credentials.access_token
+	login_session['gplus_id'] = gplus_id
+
+	# Get user info
+	userinfo_url = "https://www.googleapis.com/oauth2/v1/userinfo"
+	params = {'access_token': access_token, 'alt': 'json'}
+	answer = requests.get(userinfo_url, params=params)
+
+	data = answer.json()
+
+	login_session['username'] = data['name']
+	login_session['picture'] = data['picture']
+	login_session['email'] = data['email']
+
+	# user_id = getUserID(login_session['email'])
+	# if not user_id:
+	# 		user_id = createUser(login_session)
+
+	# login_session['user_id'] = user_id
+
+	output = ''
+	output += '<h3>Welcome, '
+	output += login_session['username']
+	output += '!</h3>'
+	output += '<img src="'
+	output += login_session['picture']
+	output += ' " style = "width: 100px; height: 100px;border-radius: 150px;-webkit-border-radius: 150px;-moz-border-radius: 150px;"> '
+	flash("you are now logged in as %s" % login_session['username'])
+	print ("done!")
+	print ("Usuario " + login_session['username'])
+	return output
+	
+
+@app.route('/gdisconnect')
+def gdisconnect():
+				# Only disconnect a connected user.
+		access_token = login_session.get('access_token')
+		if access_token is None:
+				response = make_response(
+						json.dumps('Current user not connected.'), 401)
+				response.headers['Content-Type'] = 'application/json'
+				return response
+		url = 'https://accounts.google.com/o/oauth2/revoke?token=%s' % access_token
+		h = httplib2.Http()
+		result = h.request(url, 'GET')[0]
+		if result['status'] == '200':
+				# Reset the user's sesson.
+				del login_session['access_token']
+				del login_session['gplus_id']
+				del login_session['username']
+				del login_session['email']
+				del login_session['picture']
+				response = make_response(json.dumps('Successfully disconnected.'), 200)
+				response.headers['Content-Type'] = 'application/json'
+				return redirect(url_for('showGenres'))
+		else:
+				# For whatever reason, the given token was invalid.
+				response = make_response(
+						json.dumps('Failed to revoke token for given user.', 400))
+				response.headers['Content-Type'] = 'application/json'
+		return response
 
 
 @app.route('/register/', methods=['GET'])
@@ -41,7 +172,11 @@ def valid_pw(name, password, h):
 @app.route('/', methods=['GET'])
 @app.route('/public/', methods=['GET'])
 def showMain():
-	return render_template('index.html')
+	state = ''.join(random.choice(
+			string.ascii_uppercase + string.digits) for x in range(32))
+	# store it in session for later use
+	login_session['state'] = state
+	return render_template('index.html',STATE = state)
 
 @app.route('/register/nuevousuario', methods=['POST'])
 def nuevoUsuario():
@@ -73,10 +208,12 @@ def login():
 		return render_template('main.html', username=login_session['username'])
 
 	else:
-		error = "Usuario no registrado!!!"
+		error = "Usuario no registrado"
 		return render_template('index.html', error = error)
 	
-	
+@app.route('/home/',methods=['GET','POST'])
+def ShowHome():
+		return render_template('main.html', username=login_session['username'])
 	
 
 
